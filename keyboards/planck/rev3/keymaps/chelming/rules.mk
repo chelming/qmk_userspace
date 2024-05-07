6f7dc5ee85a8e51956714446fda808f783568b3c
Chris Helming <chris.helming@gmail.com>
2024-05-07 14:34:56 -0400

    adding base keymap

--- keyboards/planck/rev3/keymaps/chelming/rules.mk
+++ keyboards/planck/rev3/keymaps/chelming/rules.mk
@@ -1,3 +1,9 @@
 ifeq ($(strip $(AUDIO_ENABLE)), yes)
     SRC += muse.c
 endif
+
+ifneq ($(strip $(NO_SECRETS)), yes)
+    ifneq ("$(wildcard $(USER_PATH)/secrets.c)","")
+        SRC += secrets.c
+    endif
+endif
